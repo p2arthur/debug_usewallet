7ef1f6eef819973feafc116ba862d7d9fd4ff15d
Arthur Rabelo <arthurrabelopro@gmail.com>
2024-01-12 15:58:39 -0300

    (styling) start styling new carroussel card

--- components/carousel/CarouselCard.vue
+++ components/carousel/CarouselCard.vue
@@ -1,26 +1,28 @@
 <!-- @format -->
 
 <script setup lang="ts">
-import { openUrl } from "../../functions/openUrl";
-const props = defineProps(["item"]);
+  import { openUrl } from '../../functions/openUrl';
+  const props = defineProps(['item']);
 </script>
 
 <template>
-  <img
-    class="carousel-item-image"
-    :src="item.src"
-    :alt="item.alt"
-    @click="openUrl(item.url)"
-  />
+  <div class="carousel-card">
+    <img
+      class="carousel-item-image"
+      :src="item.src"
+      :alt="item.alt"
+      @click="openUrl(item.url)" />
+  </div>
 </template>
 
 <style>
-.carousel-item {
-  width: 300px; /* Add any additional styling for carousel items */
-}
-.carousel-item-image {
-  object-fit: fit;
-  width: 200px;
-  height: 200px;
-}
+  .carousel-card {
+    background: red;
+    padding: 10px;
+    width: 100px; /* Add any additional styling for carousel items */
+  }
+  .carousel-item-image {
+    object-fit: fit;
+    width: 100%;
+  }
 </style>
