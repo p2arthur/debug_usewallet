f2ed2ba3a9c614ca357e25e1c1fad8af5db007f9
Arthur Rabelo <arthurrabelopro@gmail.com>
2024-02-01 22:34:59 -0300

    [upd:nitro] update nitro template for netlify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   nitro: {
-    preset: 'node-server',
+    preset: 'netlify',
     compressPublicAssets: {
       brotli: true,
     },
