475dd681fb82f4001b1fac3612f27838ea91d33a
Arthur Rabelo <arthurrabelopro@gmail.com>
2024-02-06 00:05:59 -0300

    [upd:data] update team images to dummy nodekeepers images

--- src/content/team/teamList.ts
+++ src/content/team/teamList.ts
@@ -1,10 +1,27 @@
 const teamList = [
-  { name: 'Ulrik', role: 'gamedev', image: '/img/Teampfp.jpg' },
-  { name: 'David', role: 'CEO', image: '/img/Teampfp.jpg' },
-  { name: 'Allan', role: 'backend dev', image: '/img/Teampfp.jpg' },
-  { name: 'Odi', role: '3dev', image: '/img/Teampfp.jpg' },
-  { name: 'Stef', role: 'devchain', image: '/img/Teampfp.jpg' },
-  { name: 'P2', role: 'frontend dev', image: '/img/Teampfp.jpg' },
+  { name: 'Ulrik', role: 'gamedev', image: '/img/team_pfp/nodekeeper_526.png' },
+  { name: 'David', role: 'CEO', image: '/img/team_pfp/nodekeeper_1.png' },
+  {
+    name: 'Feli',
+    role: 'C Manager',
+    image: '/img/team_pfp/nodekeeper_249.png',
+  },
+  {
+    name: 'Allan',
+    role: 'backend dev',
+    image: '/img/team_pfp/nodekeeper_191.png',
+  },
+  { name: 'Odi', role: '3dev', image: '/img/team_pfp/Teampfp.jpg' },
+  {
+    name: 'Stef',
+    role: 'devchain',
+    image: '/img/team_pfp/nodekeeper_076.png',
+  },
+  {
+    name: 'P2',
+    role: 'frontend dev',
+    image: '/img/team_pfp/nodekeeper_398.png',
+  },
 ];
 
 export default teamList;
